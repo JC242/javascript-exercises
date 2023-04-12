373dcc1837a44f33681d3bfe6f05b7ad265a680d
JC242 <jcxd24@live.com>
2023-04-12 13:22:53 -0700

    Completed Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let originalString = string.split("");
+    let reverseString = originalString.reverse().join("")
 
+    return reverseString;
 };
 
 // Do not edit below this line
