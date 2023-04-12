07935708e369ecea5ad07a94da52bc43d9cb8a9e
JC242 <jcxd24@live.com>
2023-04-12 14:27:13 -0700

    Completed Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNum,lastNum) {
+    let number = 0;
 
+    if(firstNum > lastNum){
+        let buffer = firstNum;
+        firstNum = lastNum;
+        lastNum = buffer;
+    }
+    if((typeof(firstNum) != 'number') || (typeof(lastNum) != 'number'))
+    {
+        return "ERROR";
+    }
+    if(firstNum < 0 || lastNum < 0) {return "ERROR"}
+    for(let i = firstNum; i <= lastNum ; i++){
+        number += i;
+    }
+    return number;
 };
 
 // Do not edit below this line
