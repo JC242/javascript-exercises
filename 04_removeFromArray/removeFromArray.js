7c7c78cc7cdbf9dd7123fb4af3560388daa813a5
JC242 <jcxd24@live.com>
2023-04-12 14:08:02 -0700

    Completed Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...num) {
+    
+    for(i = 0; i < num.length;i++){
+        let index = array.indexOf(num[i])
 
+        while(index > -1){
+            array.splice(index,1);
+
+            index = array.indexOf(num[i]);
+        }
+    }
+    return array;   
 };
 
 // Do not edit below this line
