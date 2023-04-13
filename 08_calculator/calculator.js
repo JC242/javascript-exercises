e81a1b81bddac9c6b697ec4dac519ff1d1f095b4
Juan <juanc.herrera@cetys.edu.mx>
2023-04-13 08:13:24 -0700

    Completed Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,52 @@
-const add = function() {
-	
+const add = function(...num) {
+  let result = 0;
+  for(let i = 0; i < num.length; i++){
+    result += num[i];
+  }
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(...num) {
+	let result = 0;
+  for(let i = 0; i < num.length; i++){
+    result = Math.abs(result - num[i]);
+
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  for(let i = 0; i < array.length; i++){
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  if(array.length < 1){ return 0;}
+  let result = array[0];
+  for(let i = 1; i < array.length; i++){
+    result = result * array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(...num) {
+	let result = num[0];
+  for(let i =  1; i < num.length; i++){
+    result = result ** num[i];
+  }
+  return result;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if(num == 0) { return 1; }
+  let result = 1;
+  for(let i = 1; i <= num; i++){
+    result = result * i; 
+  }
+  return result;
 	
 };
 
