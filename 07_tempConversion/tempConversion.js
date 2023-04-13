463341d2425991cc70b52bfe7df672837a7acecd
Juan <juanc.herrera@cetys.edu.mx>
2023-04-13 07:31:48 -0700

    Completed Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  let celsius = (temp - 32) * (5 / 9);
+  return round(celsius);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp * (9 / 5))  + 32;
+  return round(fahrenheit);
 };
+
+const round = (num) =>{
+  num  = Math.round(num * 10) / 10 ;
+  return num;
+}
 
 // Do not edit below this line
 module.exports = {
