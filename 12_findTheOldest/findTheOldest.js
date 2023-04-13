d1d42fc99803d52d537faa3c26c128db08dc93f2
Juan <juanc.herrera@cetys.edu.mx>
2023-04-13 13:51:58 -0700

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let buffer = 0;
+    let Oldperson = []
+    for(let i = 0; i < array.length; i++){
+        for(let a = 0; a < array.length;a++){
+            if(array[a].hasOwnProperty('yearOfDeath') == false){
+                DateNow = new Date().getFullYear();
+            }else{
+                DateNow = array[a].yearOfDeath;
+            }
+            let age = DateNow - array[a].yearOfBirth;
+            if(age > buffer){
+                buffer = age;
+                Oldperson = array[a]
+            }
+        }
+    }
+    return Oldperson;
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
