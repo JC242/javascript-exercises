2a175056aff8b77b9f95506ac9a76ebf821e50c4
Juan <juanc.herrera@cetys.edu.mx>
2023-04-13 10:43:04 -0700

    Completed exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = parseInt(num);
+    if(Math.sign(num) == -1){ return "OOPS"}
+    let numArray = [];
+    let result = 0;
+    for(let i = 0; i < num; i++){
+        if(i == 0){
+            result = 1;
+        }else{
+            if(numArray[i-2] === undefined){
+                result = numArray[i-1]
+            }else{
+                result = numArray[i-2];
+                result = result + numArray[i-1];
+            }
+        }
+        numArray.push(result);
+    }
+    return numArray[num - 1];
 };
 
 // Do not edit below this line
